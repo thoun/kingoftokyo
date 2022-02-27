791abb6de880b47dd86be5384764eed88878d981
Guy Baudouin <guythoun@gmail.com>
2022-02-27 11:57:10 +0100

    fix logs notice

--- modules/php/curse-cards/curse-cards-utils.php
+++ modules/php/curse-cards/curse-cards-utils.php
@@ -251,7 +251,7 @@ trait CurseCardsUtilTrait {
     function getPlayerIdWithGoldenScarab() {
         $playerId = intval($this->getGameStateValue(PLAYER_WITH_GOLDEN_SCARAB));
 
-        if (!$playerId || $this->getPlayer($playerId)->eliminated) {
+        if ($playerId == 0 || $this->getPlayer($playerId)->eliminated) {
             return null;
         }
 
